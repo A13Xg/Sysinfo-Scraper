9acf63dce32d9f81e289fe1b39b120452de5ee6b
A13Xg <38967711+A13Xg@users.noreply.github.com>
2019-02-06 15:02:41 -0800

    Added via Version 0.2

--- Scrape Info.bat
+++ Scrape Info.bat
@@ -5,10 +5,15 @@ REM Signed by @13X
 echo Current User: %USERNAME% >> sysinfo_%USERNAME%_%COMPUTERNAME%.TXT
 
 echo Computer Name: %COMPUTERNAME% >> sysinfo_%USERNAME%_%COMPUTERNAME%.TXT
-
-echo Current IP Configuration: >> sysinfo_%USERNAME%_%COMPUTERNAME%.TXT
-ipconfig /all >> sysinfo_%USERNAME%_%COMPUTERNAME%.TXT
+echo "            " >> sysinfo_%USERNAME%_%COMPUTERNAME%.TXT
+echo "<-------------------------------------->" >> sysinfo_%USERNAME%_%COMPUTERNAME%.TXT
+echo "            " >> sysinfo_%USERNAME%_%COMPUTERNAME%.TXT
 
 echo System Info: >> sysinfo_%USERNAME%_%COMPUTERNAME%.TXT
 Systeminfo >> sysinfo_%USERNAME%_%COMPUTERNAME%.TXT
+echo "            " >> sysinfo_%USERNAME%_%COMPUTERNAME%.TXT
+echo "<-------------------------------------->" >> sysinfo_%USERNAME%_%COMPUTERNAME%.TXT
+echo "            " >> sysinfo_%USERNAME%_%COMPUTERNAME%.TXT
 
+echo Current IP Configuration: >> sysinfo_%USERNAME%_%COMPUTERNAME%.TXT
+ipconfig /all >> sysinfo_%USERNAME%_%COMPUTERNAME%.TXT
